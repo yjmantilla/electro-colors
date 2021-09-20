c6005033c410d0a241338ad94f099998b3d81a6d
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-09-20 13:18:09 -0500

    final comments

--- analysis.py
+++ analysis.py
@@ -1,35 +1,54 @@
-import os
-from glob import glob
-import pandas as pd
-from copy import deepcopy
-import matplotlib.pyplot as plt
-from pandas.core import groupby
-
-PATH = '.output'
-files = [y for x in os.walk(PATH) for y in glob(os.path.join(x[0], '*.csv'))]
-
-dfs = []
-file_metrics = []
-for f in files:
-    print(f)
-    df = pd.read_csv(f,sep=',')
-    cols = deepcopy(df.columns)
-    df = df.drop(cols[0], axis=1)
-    df = df.drop(cols[1], axis=1)
-    file=[f]*df.shape[0]
-    df['file']=file
-    dfs.append(deepcopy(df))
-    worst_corr=df['label_corr'].min()
-    worst_dist=df['color_dist'].max()
-    bad_label=','.join(df.query(f'label_corr == {worst_corr}')['label'].tolist())
-    bad_color =','.join(df.query(f'color_dist == {worst_dist}')['label'].tolist())
-    bads = 'bad_label = ' + bad_label +' ---- bad_color = ' + bad_color
-    file_metrics.append(pd.DataFrame({'worst_corr':worst_corr,'worst_dist':worst_dist,'suspects':bads},index=[f]))
+#--------------------------------------------------------------------------------------------------
+#--------------------Extraccion de impedancias en escala de color----------------------------------
+#--------------------------------------------------------------------------------------------------
+#-------------Alexis Rafael del Carmen Ávila Ortiz--------CC 1083555169----------------------------
+#-------------alexis.avila@udea.edu.co--------------------Wpp +57 305 2230574----------------------
+#--------------------------------------------------------------------------------------------------
+#-------------Yorguin José Mantilla Ramos-----------------CC 1127617499----------------------------
+#-------------yorguinj.mantilla@udea.edu.co---------------Wpp +57 311 5154452----------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#----------------------Estudiantes Facultad de Ingenieria  ----------------------------------------
+#--------Curso Básico de Procesamiento de Imágenes y Visión Artificial-----------------------------
+#--------------------------Septiembre de 2021------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
 
-the_df = pd.concat(dfs)
-electrodes=the_df['label'].unique()
+import os                           # Para manipulacion de rutas
+from glob import glob               # Para listado de archivos
+import pandas as pd                 # Para manipulacion de tablas
+from copy import deepcopy           # Para copias profundas de variables
+import matplotlib.pyplot as plt     # Para graficacion
 
+PATH = '.output'                    # Carpeta de resultados del software
+files = [y for x in os.walk(PATH) for y in glob(os.path.join(x[0], '*.csv'))] # Busqueda de tablas csv
+
+dfs = []                            # Creamos un solo dataframe con las tablas de todos los archivos
+file_metrics = []                   # Variable para guardar metricas de peor correlacion y distancia de cada archivo
+
+# Recoleccion de resultados archivo por archivo
+for f in files:                                                                                                         # Para cada archivo
+    print(f)                                                                                                            # Para ver por donde va...
+    df = pd.read_csv(f,sep=',')                                                                                         # Importar la tabla
+    cols = deepcopy(df.columns)                                                                                         # Extraer columnas de la tabla
+    df = df.drop(cols[0], axis=1)                                                                                       # Quitar columna de indice
+    df = df.drop(cols[1], axis=1)                                                                                       # Quitar columna de numero de electrodo
+    file=[f]*df.shape[0]                                                                                                # Crear vector extendido para asociar archivo de origen a cada datum de electrodo
+    df['file']=file                                                                                                     # Expandir tabla con el vector extendido anterior
+    dfs.append(deepcopy(df))                                                                                            # Crear copia no mutable en la lista acumladora
+    worst_corr=df['label_corr'].min()                                                                                   # Peor correlacion del archivo
+    worst_dist=df['color_dist'].max()                                                                                   # Peor distancia del archivo
+    bad_label=','.join(df.query(f'label_corr == {worst_corr}')['label'].tolist())                                       # Identificar electrodos asociadoas a la peor correlacion
+    bad_color =','.join(df.query(f'color_dist == {worst_dist}')['label'].tolist())                                      # Identificar electrodos asociados a la peor distancia
+    bads = 'bad_label = ' + bad_label +' ---- bad_color = ' + bad_color                                                 # String con los electrodos sospechosos de cada archivo
+    file_metrics.append(pd.DataFrame({'worst_corr':worst_corr,'worst_dist':worst_dist,'suspects':bads},index=[f]))      # Agregar peor correlacion y distancia a la lista
+
+
+the_df = pd.concat(dfs)                                                             # Creacion del dataframe con todas las tablas
+electrodes=the_df['label'].unique()                                                 # Electrodos unicos
+
+# Boxplot de la distribucion de la peor correlacion a lo largo de los electrodos
 the_df.boxplot('label_corr',rot=45,by='label',figsize=(2*len(electrodes),3))
 plt.xlabel('Etiqueta')
 plt.ylabel('Correlacion')
@@ -37,6 +56,7 @@ plt.suptitle('')
 plt.title('Distribuciones de la correlacion a lo largo de las etiquetas')
 plt.show()
 
+# Boxplot de la distribucion de la peor distancia a lo largo de los electrodos
 the_df.boxplot('color_dist',rot=45,by='label',figsize=(2*len(electrodes),3))
 plt.xlabel('Etiqueta')
 plt.ylabel('Distancia euclideana entre colores')
@@ -44,53 +64,37 @@ plt.suptitle('')
 plt.title('Distribuciones de la distancia entre colores a lo largo de las etiquetas')
 plt.show()
 
-file_df = pd.concat(file_metrics)
-file_df.boxplot(['worst_corr'])
-plt.show()
 
-file_df = pd.concat(file_metrics)
-file_df.boxplot(['worst_dist'])
-plt.show()
+# Analisis de la frecuencia de condiciones no aceptables (archivos sospechosos de error)
 
-import numpy as np
-min_acceptable_corr = 0.6
-max_acceptable_dist = 10
-normalize = True #False
-worst_corr = file_df['worst_corr'].apply(lambda x: 'corr >= 0.6' if x >= 0.6 else 'corr < 0.6').value_counts(normalize=normalize)
-worst_corr = worst_corr.round(decimals = 3)
-worst_corr = worst_corr.apply(lambda x: x*100)
-#worst_corr.index = worst_corr.index.to_series()
-worst_corr.value_counts(normalize=normalize)
-worst_corr.plot.pie(autopct='%1.2f%%')
+file_df = pd.concat(file_metrics)                       # Dataframe de peores correlaciones y peores distancias a lo largo de los archivos
+min_acceptable_corr = 0.6                               # Minima Correlacion Aceptable
+max_acceptable_dist = 10                                # Maxima Distancia Aceptable
+normalize = True                                        # Normalizacion de frecuencias (usa porcentajes en vez de la cantidad de veces)
 
 
+# Grafica de frecuencia de la peor correlacion siendo mas baja que la minima aceptable
+worst_corr = file_df['worst_corr'].apply(lambda x: 'corr >= 0.6' if x >= 0.6 else 'corr < 0.6').value_counts(normalize=normalize)       # Particion sobre cumple o no cumple la condicion aceptable
+worst_corr = worst_corr.round(decimals = 3)                                                                                             # Redondeo de frecuencias
+worst_corr = worst_corr.apply(lambda x: x*100)                                                                                          # Conversion a porcentaje
+worst_corr.plot.pie(autopct='%1.2f%%')                                                                                                  # Grafica
 plt.ylabel('')
 plt.suptitle('')
 plt.title('Frecuencia de la peor correlacion de una imagen')
 plt.show()
 
 
-
-
-worst_dist = file_df['worst_dist'].apply(lambda x: 'dist >= 10' if x >= 10 else 'dist < 10').value_counts(normalize=normalize)
-
-worst_dist = worst_dist.round(decimals = 3)
-worst_dist = worst_dist.apply(lambda x: x*100)
-#worst_dist.index = worst_dist.index.to_series().apply(lambda x: np.round(x,3))
-worst_dist.plot.pie(autopct='%1.2f%%')
-
+# Grafica de frecuencia de la peor distancia siendo mas alta que la maxima aceptable
+worst_dist = file_df['worst_dist'].apply(lambda x: 'dist >= 10' if x >= 10 else 'dist < 10').value_counts(normalize=normalize)          # Particion sobre cumple o no cumple la condicion aceptable
+worst_dist = worst_dist.round(decimals = 3)                                                                                             # Redondeo de frecuencias
+worst_dist = worst_dist.apply(lambda x: x*100)                                                                                          # Conversion a porcentaje
+worst_dist.plot.pie(autopct='%1.2f%%')                                                                                                  # Grafica
 plt.ylabel('')
 plt.suptitle('')
 plt.title('Frecuencia de la peor distancia de una imagen')
 plt.show()
 
 
-# Busqueda de archivos con mayor probabilidad de error
-
-
-
-suspect_files = file_df.query(f'worst_corr <= {min_acceptable_corr} or worst_dist >= {max_acceptable_dist}')
-
-suspect_files.shape
-
-suspect_files.to_html(os.path.join(PATH,'suspect_files.html'))
+# Busqueda de archivos con mayor probabilidad de error (Archivos Sospechosos)
+suspect_files = file_df.query(f'worst_corr <= {min_acceptable_corr} or worst_dist >= {max_acceptable_dist}')                            # Busqueda de los archivos donde alguna de las dos condiciones se incumpla
+suspect_files.to_html(os.path.join(PATH,'suspect_files.html'))                                                                          # Guardar lista de archivos sospechosos
--- run.py
+++ run.py
@@ -1,39 +1,55 @@
-import os
-import shutil
-from zcolors.impedance import process_image,process_example
-from zcolors.utils import _get_files
-from traceback import format_exc
-import json
 #--------------------------------------------------------------------------------------------------
-#-------------Variables de entrada-----------------------------------------------------------------
+#--------------------Extraccion de impedancias en escala de color----------------------------------
+#--------------------------------------------------------------------------------------------------
+#-------------Alexis Rafael del Carmen Ávila Ortiz--------CC 1083555169----------------------------
+#-------------alexis.avila@udea.edu.co--------------------Wpp +57 305 2230574----------------------
+#--------------------------------------------------------------------------------------------------
+#-------------Yorguin José Mantilla Ramos-----------------CC 1127617499----------------------------
+#-------------yorguinj.mantilla@udea.edu.co---------------Wpp +57 311 5154452----------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#----------------------Estudiantes Facultad de Ingenieria  ----------------------------------------
+#--------Curso Básico de Procesamiento de Imágenes y Visión Artificial-----------------------------
+#--------------------------Septiembre de 2021------------------------------------------------------
 #--------------------------------------------------------------------------------------------------
 
-ROOT_DIR = r"Y:\code\electro-colors\.anon"#'.images_source'                # Directorio donde se encuentran las imagenes originales
-                                    # que se van a procesar.
-OUTPUT_DIR = '.output'                # Directorio donde se va a guardar el resultado de procesamiento
-                                    # de cada una de las imagenes.
 
-SAVE_IMAGES = True                  # Si salvar o no las imagenes de los electrodos individuales
+import os                                                           # Para manipulacion de rutas
+import shutil                                                       # Para eliminacion de carpetas
+from zcolors.impedance import process_image,process_example         # Para el Procesamiento de Imagenes
+from zcolors.utils import _get_files                                # Para obtener listado de archivos
+from traceback import format_exc                                    # Para formateo de excepciones
+import json                                                         # Para guardar archivo json
 
-EXAMPLE = 'images/6.png'  # La imagen de ejemplo debe tener TODOS los electrodos (ojo con VEO y HEO, ambos deben tener 2 colores contrastados para asegurar buen funcionamiento)
+
+ROOT_DIR = r"Y:\code\electro-colors\.anon"                          # Directorio donde se encuentran las imagenes a procesar
+OUTPUT_DIR = '.output'                                              # Directorio donde se van a guarda los resultados de procesamiento
+SAVE_IMAGES = True                                                  # Define si salvar o no las imagenes de los electrodos individuales
+EXAMPLE = 'images/6.png'                                            # La imagen de ejemplo. 
+                                                                    # Esta debe tener TODOS los electrodos (ojo con VEO y HEO, ambos deben tener 2 colores contrastados para asegurar buen funcionamiento)
+
+# Etiquetas de los electrodos, deben tener el mismo orden en el que los electrodos se segmentan por get_electrodes
 LABELS = ['O1','O2','OZ','PO8','PO7','PO5','PO6','PO4','PO3','POZ','P8','P7','P6','P5','P3','P4','P2','PZ','P1','TP8','TP7','CP6','CP5','CP4','CP2','CPZ','CP1','CP3','C2','CZ','C1','C3','C5','T7','C6','C4','T8','FC2','FCZ','FC4','FC1','FC3','FC6','F1','F2','F4','F3','F5','FC5','F6','FZ','F8','F7','AF3','AF4','FP2','FP1','FPZ','VEO','HEO']
 
-if __name__ == '__main__':                                  # Funcionamiento del programa sobre una carpeta
-    listOfFiles = _get_files(ROOT_DIR)        # Lista de archivos a procesar
-    if os.path.isdir(OUTPUT_DIR):                           # Revisar si el directorio de salida ya existe
-        shutil.rmtree(OUTPUT_DIR)                           # Limpiarlo en ese caso
+if __name__ == '__main__':
+    listOfFiles = _get_files(ROOT_DIR)                              # Lista de archivos a procesar
+    if os.path.isdir(OUTPUT_DIR):                                   # Revisar si el directorio de salida ya existe
+        shutil.rmtree(OUTPUT_DIR)                                   # Limpiarlo en ese caso
 
-    labeled_electrodes=process_example(EXAMPLE,LABELS,OUTPUT_DIR,SAVE_IMAGES)
-    errors=[]
-    for i,image in enumerate(listOfFiles):
+    labeled_electrodes=process_example(EXAMPLE,LABELS,OUTPUT_DIR,SAVE_IMAGES)   # Segmentar y Etiquetar ejemplo siguiendo las etiquetas de entrada
+    errors=[]                                                                   # Variable acumuladora de errores
+
+    for i,image in enumerate(listOfFiles):                                      # Procesamiento iterativo imagen a imagen
         try:
-            output = os.path.join(OUTPUT_DIR,os.path.split(image.replace(ROOT_DIR,''))[0].strip('/').strip('\\'))
-            process_image(image,labeled_electrodes,output,SAVE_IMAGES)
-            print(i)
+            output = os.path.join(OUTPUT_DIR,os.path.split(image.replace(ROOT_DIR,''))[0].strip('/').strip('\\'))   # Definicion de la carpeta de salida para la imagen actual
+            process_image(image,labeled_electrodes,output,SAVE_IMAGES)                                              # Procesamiento de la imagen
+            print(i)                                                                                                # Contador para saber por cual imagen va...
         except:
-            errors.append({'file':image,'error':format_exc()})
-            print('error',i)
-    with open(os.path.join(OUTPUT_DIR,'errors.txt'), 'w') as outfile:
+            errors.append({'file':image,'error':format_exc()})                                                      # Logging de errores
+            print('error',i)                                                                                        # Impresion de errores
+
+    with open(os.path.join(OUTPUT_DIR,'errors.txt'), 'w') as outfile:                                               # Guardar historial de errores
         json.dump(errors, outfile,indent=4)
 
     print('END')
